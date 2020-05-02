ba91ade23864ccbe376457126e24c72949fecc09
RichardCastillo <armed_deity@hotmail.com>
2020-05-02 09:34:44 -0700

    Initial commit

---
+++ src/edu/whccd/rcastillo/Main.java
@@ -0,0 +1,24 @@
+package edu.whccd.rcastillo;
+
+import java.util.Scanner;
+
+/* This assignment is used to apply methods and data validation
+    Author: Richard Castillo
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class displayString {
+        public string = input;
+        System.out.println()
+    }
+    public class isValid {
+
+    }
+
+
+}
