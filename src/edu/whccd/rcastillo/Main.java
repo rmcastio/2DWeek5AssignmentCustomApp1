70f6080dc8b10370790a0b430d66c92030cafba3
RichardCastillo <armed_deity@hotmail.com>
2020-05-04 17:43:30 -0700

    added up to 9.5 in homework.

--- src/edu/whccd/rcastillo/Main.java
+++ src/edu/whccd/rcastillo/Main.java
@@ -8,17 +8,43 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void displayString(String input) {
+        System.out.println(input);
+    }
+
+    static boolean isValid (in input, int lowerBound, int upperBound) {
+        System.out.print("This Method validates if integer input is within range.");
+        boolean output = false;
+        if (input >= lowerBound && input <= upperBound) {
+            output = true;
+            displayString(input + "Within range");
+        } else {
+            displayString(input + "Not within Range");
+        }
+        return output;
+    }
+
+    static boolean isValid(double input, double lowerBound, double upperBound) {
+        System.out.print("This Method validates if double input is within range.");
+        boolean output = false;
+        if (input >= lowerBound && input <= upperBound) {
+            output = true;
+            displayString(input + "Within range");
+        } else {
+            displayString(input + "Not within Range");
+        }
+        return output;
+    }
+
     public static void main(String[] args) {
+        int lowerBound = 100;
+        int upperBound = 1000;
+        boolean isValidInteger = false;
+        boolean isValidDoubleInput = false;
 
+        Scanner sc = new Scanner(System.in);
     }
-
-    public class displayString {
-        public string = input;
-        System.out.println()
+    do {
+        displayString();
     }
-    public class isValid {
-
-    }
-
-
 }
